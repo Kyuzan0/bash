e025597b793f62147cface201c63073cf9b2b625
DuckTail <154976490+TrDuckTail@users.noreply.github.com>
2024-07-15 19:28:52 +0700

    Update install-mikrotik.sh
    why echo missing

--- install-mikrotik.sh
+++ install-mikrotik.sh
@@ -2,6 +2,6 @@
 
 clear
 
-sudo echo "downloading chr-7.15.2.img"
+echo "Downloading chr-7.15.2.img"
+echo ""
 sudo curl -L "https://drive.usercontent.google.com/download?id=1iDPyOLPPXRlb9blGTsVSoLMHKlRjG_IL&export=download&authuser=1&confirm=t&uuid=bab66514-f7f6-4542-b0e4-c5a4591e29ca&at=APZUnTXEU5DVX9k0_u6dhFubr6Uq:1721045107400" > chr.img
-
