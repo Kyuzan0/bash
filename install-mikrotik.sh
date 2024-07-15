7c530bc2d94fdc8e76878ee63ab68a451ed1a940
DuckTail <154976490+TrDuckTail@users.noreply.github.com>
2024-07-15 19:10:55 +0700

    Create install-mikrotik.sh

---
+++ install-mikrotik.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+clear
+
+sudo curl -L "https://drive.usercontent.google.com/download?id=1iDPyOLPPXRlb9blGTsVSoLMHKlRjG_IL&export=download&authuser=1&confirm=t&uuid=bab66514-f7f6-4542-b0e4-c5a4591e29ca&at=APZUnTXEU5DVX9k0_u6dhFubr6Uq:1721045107400" > chr.img
+
